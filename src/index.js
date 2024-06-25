b087d60a59c7ecbfbdafbae52085811f50107303
Jinheng Xu <xjinheng@vmware.com>
2024-06-25 15:46:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
+  "docker.2peach.com": dockerHub,
   "quay.libcuda.so": "https://quay.io",
   "gcr.libcuda.so": "https://gcr.io",
   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
+  "k8s.2peach.com": "https://registry.k8s.io",
   "ghcr.libcuda.so": "https://ghcr.io",
   "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
   "ecr.libcuda.so": "https://public.ecr.aws",
